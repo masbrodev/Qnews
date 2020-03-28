ce5fa9c1ca551f9f844d69ba3239894ce941e1e2
Masbro <s2gotosingapore@gmail.com>
2020-03-28 23:37:18 +0700

    tambah fungsi Login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,4 +27,23 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function index()
+    {
+        if (Auth::user()->email == "admin@portal.com") {
+            $date  = new DateTime;
+            $today = $date->format('d');
+            $month = $date->format('m') - 1;
+            $year  = $date->format('Y');
+
+            $total = Visitor::all()->count();
+            $hari  = Visitor::whereDay('created_at', $today)
+                ->whereMonth('created_at', $month)
+                ->whereYear('created_at', $year)->count();
+
+            $bulan = Visitor::whereMonth('created_at', $month)
+                ->whereYear('created_at', $year)->count();
+
+            return view('admin.home', ['hari' => $hari, 'bulan' => $bulan, 'total' => $total]);
+        } else return view('user.home');
+    }
 }
