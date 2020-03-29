9ee5c331a425ab20b54931cefd62cf7defda9e68
Masbro <s2gotosingapore@gmail.com>
2020-03-29 23:43:18 +0700

    penampilkan komentar

--- resources/views/user/comment.blade.php
+++ resources/views/user/comment.blade.php
@@ -1,3 +1,13 @@
 @extends('layouts.admin')
 @section('content')
+
+	<div class="col-10 col-lg-8 main-admin">
+		<div class="title-admin pbot">Daftar Komentar</div>
+		<div class="col-12 pic-content bbotn mtop">
+				<div class="col-12 main-val">
+						</div>
+					@endforeach
+				</div>
+		</div>
+	</div>
 @endsection
