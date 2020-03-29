8c6d64fb048ca7b0588e9e25b53606a2a92d2d2e
Masbro <s2gotosingapore@gmail.com>
2020-03-29 23:46:01 +0700

    import segalanya

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Portal Berita</title>
+	<link rel="shortcut icon" href="/img/newspaper.png"> 
+	<link rel="stylesheet" type="text/css" href="/css/rewidify.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<script src="/tinymce/tinymce.min.js"></script>
+	<script src="/js/jquery3.min.js"></script>
+	<script src="/js/script.js"></script>
+</head>
 
 </body>
 </html>
