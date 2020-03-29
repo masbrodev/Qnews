98edffa112eaa21d43a962d03b96fee5ff7fd691
Masbro <s2gotosingapore@gmail.com>
2020-03-29 23:43:37 +0700

    Tabel komen

--- resources/views/user/comment.blade.php
+++ resources/views/user/comment.blade.php
@@ -5,6 +5,15 @@
 		<div class="title-admin pbot">Daftar Komentar</div>
 		<div class="col-12 pic-content bbotn mtop">
 				<div class="col-12 main-val">
+					<div class="col-8">{{ Session::get('msg') }}</div>
+					@foreach ($comments as $comment)
+						<div class="col-12 val-content subval-content pbot">
+							<div class="col-10 col-lg-8 col-md-6 col-sm-4 col-xs-2 sub-main">
+								<div class="col-12">{{ $comment->isi }}</div>
+								<div class="col-12 ket-com">
+									<?php $controller->fullTime($comment->created_at); ?>
+									<div class="col-9">Pada : <a href="/berita/{{ $comment->path }}">{{ $comment->judul }}</a></div>
+								</div>
 						</div>
 					@endforeach
 				</div>
