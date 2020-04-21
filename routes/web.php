9174e002d73b6ad76f65bd2b4cd5e39fd44ef078
Masbro <s2gotosingapore@gmail.com>
2020-04-21 22:31:40 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,23 @@ Route::post('/cari',		  'MainController@cari');
 Route::get('/tambah',         'AdminController@tambah');
 Route::post('/addPost',       'AdminController@addPost');
 Route::post('/editPost/{id}', 'AdminController@editPost');
+Route::get('/daftar',         'AdminController@daftar');
+Route::get('/edit/{id}',      'AdminController@edit');
+Route::get('/hapus/{id}',	  'AdminController@hapus');
+Route::get('/user',           'AdminController@user');
+Route::get('/komentar',		  'AdminController@komentar');
 Route::get('/hapusUser/{id}', 'AdminController@hapusUser');
+
+/* -------------- User-----------*/
+
+Route::post('/userInfo', 	'UserController@userInfo');
+Route::post('/addComment',  'UserController@addComment');
+Route::get('/comment', 		'UserController@comment');
+Route::get('/editComment/{id}',    'UserController@editComment');
+Route::post('/updateComment/{id}', 'UserController@updateComment');
+Route::get('/deleteComment/{id}',  'UserController@deleteComment');
+Route::post('/editBiodata', 	   'UserController@editBiodata');
+Route::post('/reply',			   'UserController@reply');
+Route::post('/like',			   'UserController@like');
+Route::post('/unlike',			   'UserController@unlike');
+Route::get('/notification', 	   'UserController@notification');
